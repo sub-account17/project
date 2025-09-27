a372716b049460a53475c0674561c1eafd82a8bc
sub-account17 <hlollolollollol@gmail.com>
2025-09-27 19:21:27 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import {mainTeg} from "./components/mainPage.js";
+import {resultTeg} from "./components/resultPage.js";
 
-document.querySelector('.MainPage').innerHTML = mainTeg;
+const url = new URL(window.location.href);
+const params = url.searchParams;
+window.history.replaceState({}, '', `${url.pathname}`);
+function pageLoad(page) {
+  document.querySelector('.MainPage').innerHTML = page == 0 ? mainTeg : "";
+  document.querySelector('.ResultPage').innerHTML = page == 1 ? resultTeg : "";
+}
+pageLoad(0);
 
 let content = {morning: "", working: ""};
 document.getElementById('morning').onchange = (e) => {
@@ -10,3 +18,9 @@ document.getElementById('working').onchange = (e) => {
   content.working = e.target.value;
 }
 
+document.getElementsByClassName('Post')[0].addEventListener("click", () => {
+  pageLoad(1);
+  
+  params.set('page', 'result');
+  window.history.replaceState({}, '', `${url.pathname}?${params}`);
+});
