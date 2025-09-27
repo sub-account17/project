f102dde446a7df74c9c85064d561b22388dda2e7
sub-account17 <hlollolollollol@gmail.com>
2025-09-27 15:15:52 +0900

    Create mainPage.js

---
+++ components/mainPage.js
@@ -0,0 +1,17 @@
+const mainTeg = `
+<div class="Profile">
+</div>
+<p>상병 김종현</p>
+<p>2중대 3소대 1분대</p>
+<h1 style="margin: 0;">2025년 09월 26일</h1>
+<h4 style="margin: 0;">아침 점호 열외</h4>
+<input type="text" class ="Content" id="morning" onchange="mInput">
+<h4 style="margin: 0;">일과 열외</h4>
+<input type="text" class="Content" id="working" onchange="wInput">
+</div>
+<div class="Post">
+<h3> 등록 </h3>
+</div>
+`;
+
+export {mainTeg};
